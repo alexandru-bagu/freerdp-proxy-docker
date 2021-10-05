9652ad9810b47270e34f428c1ba22ab3688840f9
Alexandru Bagu <alexandru.bagu@gmail.com>
2021-10-05 17:17:13 -0400

    use existing certificates

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,11 @@ if ! [ -d certificates ]; then
   winpr-makecert -silent -path certificates -n rdp-server -y 100
   winpr-makecert -silent -path certificates -n rdp-private
 fi
+if [ -f "$CERTIFICATE_CRT" ] && [ -f "$CERTIFICATE_KEY" ]; then
+  sed -i.bak "s|^CertificateFile=.*|CertificateFile=$CERTIFICATE_CRT|g" config.ini
+  sed -i.bak "s|^PrivateKeyFile=.*|PrivateKeyFile=$CERTIFICATE_KEY|g" config.ini
+else
+  sed -i.bak "s|^CertificateFile=.*|CertificateFile=/root/certificates/rdp-server.crt|g" config.ini
+  sed -i.bak "s|^PrivateKeyFile=.*|PrivateKeyFile=/root/certificates/rdp-server.key|g" config.ini
+fi
 /usr/local/bin/freerdp-proxy config.ini
--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 docker build . -t bssone/freerdp-proxy-guacamole-auth:latest
-docker run -it -p 3389:3389/tcp bssone/freerdp-proxy-guacamole-auth:latest
+docker run -v "$(pwd)/cert:/external" \
+  -e "CERTIFICATE_CRT=/external/cert.pem" \
+  -e "CERTIFICATE_KEY=/external/key.pem" \
+  -it -p 3389:3389/tcp bssone/freerdp-proxy-guacamole-auth:latest
